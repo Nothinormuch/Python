ee14ada818a396207666c2897231feb6439c2e93
Nothinormuch <nothin.main@outlook.com>
2026-09-09 12:31:28 +0530

    fix: make the code readable and remove unnecessary code

--- BasicProblems/_0019_detect_subsequences_with_sum_k_of_array_of_only_positive_nums_using_recursion.py
+++ BasicProblems/_0019_detect_subsequences_with_sum_k_of_array_of_only_positive_nums_using_recursion.py
@@ -1,22 +1,21 @@
 def detectSub(arr,target,i=0,total=0,result=[],sub=[]):
+    if(total == target):
+        return True
+    if(total>target):
+        return False
     if(i==len(arr)):
         return False
     
     sub.append(arr[i])
     total+=arr[i]
-    if(target<=total):
-        if(target==total):
-            return True
-        total-=arr[i]
-        sub.pop()
-        return False
     
     res1 = detectSub(arr,target,i+1,total,result,sub)
+    if res1==True: return res1
     total-=arr[i]
     sub.pop()
 
     res2 = detectSub(arr,target,i+1,total,result,sub)
-    return res1 or res2
+    return res2
 
 
-print(detectSub([1,2,3,4,5,6,7],50))
+print(detectSub([1,2,3,4,5,6,7],10))
